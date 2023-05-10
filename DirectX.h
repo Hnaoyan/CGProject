e93cd1487c9d0cd99be46020c302bf7a302b1df4
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-05-10 15:11:14 +0900

    02_04完了

--- DirectX.h
+++ DirectX.h
@@ -6,33 +6,33 @@
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 
-class DirectX
-{
-public:
-	//初期化
-	void Initialize(HWND& hwnd);
-
-	void DXGIDevice(HWND& hwnd);
-
-	void InitializeCommand();
-
-	void SwapChain(HWND& hwnd);
-
-	void Set();
-
-	void RenderTargetView(HRESULT& hr, ID3D12Device* device, IDXGISwapChain4* swapChain,
-		ID3D12GraphicsCommandList* commandList, ID3D12CommandQueue* commandQueue, ID3D12CommandAllocator* commandAllocator);
-
-public:
-	//HRESULT hr;
-	//IDXGIFactory7* dxgiFactory = nullptr;
-	//ID3D12Device* device = nullptr;
-	//IDXGIAdapter4* useAdapter = nullptr;
-	//ID3D12CommandQueue* commandQueue = nullptr;
-	//ID3D12CommandAllocator* commandAllocator = nullptr;
-	//ID3D12GraphicsCommandList* commandList = nullptr;
-	//IDXGISwapChain4* swapChain = nullptr;
-	//ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
-	//ID3D12Resource* swapChainResources[2] = { nullptr };
-};
+//class DirectX
+//{
+//public:
+//	//初期化
+//	void Initialize(HWND& hwnd);
+//
+//	void DXGIDevice(HWND& hwnd);
+//
+//	void InitializeCommand();
+//
+//	void SwapChain(HWND& hwnd);
+//
+//	void Set();
+//
+//	void RenderTargetView(HRESULT& hr, ID3D12Device* device, IDXGISwapChain4* swapChain,
+//		ID3D12GraphicsCommandList* commandList, ID3D12CommandQueue* commandQueue, ID3D12CommandAllocator* commandAllocator);
+//
+//public:
+//	//HRESULT hr;
+//	//IDXGIFactory7* dxgiFactory = nullptr;
+//	//ID3D12Device* device = nullptr;
+//	//IDXGIAdapter4* useAdapter = nullptr;
+//	//ID3D12CommandQueue* commandQueue = nullptr;
+//	//ID3D12CommandAllocator* commandAllocator = nullptr;
+//	//ID3D12GraphicsCommandList* commandList = nullptr;
+//	//IDXGISwapChain4* swapChain = nullptr;
+//	//ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
+//	//ID3D12Resource* swapChainResources[2] = { nullptr };
+//};
 
