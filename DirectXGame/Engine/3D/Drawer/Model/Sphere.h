afb3f7a20586ef20d4e585a4c9fbcdb38e06df7e
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-01-27 19:04:47 +0900

    include

--- DirectXGame/Engine/3D/Drawer/Model/Sphere.h
+++ DirectXGame/Engine/3D/Drawer/Model/Sphere.h
@@ -1,5 +1,31 @@
 #pragma once
+#include "imgui.h"
+#include "imgui_impl_dx12.h"
+#include "imgui_impl_win32.h"
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+#include <dxgidebug.h>
+#pragma comment(lib,"dxguid.lib")
+
+#include <dxcapi.h>
+#pragma comment(lib, "dxcompiler.lib")
+
+#include<d3d12.h>
+#include<dxgi1_6.h>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+#include "DirectXTex.h"
+#include "d3dx12.h"
+
+
 class Sphere
 {
+public:
+
+private:
+
+
 };
 
